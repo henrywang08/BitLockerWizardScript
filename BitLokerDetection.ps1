2ef568870b1dc704843b188df3a707974e37f68b
Henry Wang <henry.wang@microsoft.com>
2023-02-21 19:26:43 +0800

    Update the script

--- BitLokerDetection.ps1
+++ BitLokerDetection.ps1
@@ -1,11 +1,35 @@
-﻿$BLDriveC = Get-BitLockerVolume c:
+﻿$LogFile = "c:\windows\temp\bldetect.log"
+
+Function LogMessage($msg)
+{
+    $Timestamp = get-date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "$Timestamp $msg" 
+    "$Timestamp $msg" |     Out-File -Append -FilePath $LogFile 
+}
+
+$Message = "Start bitlocker detection on C: drive"
+LogMessage($Message)
+
+$BLDriveC = Get-BitLockerVolume c:
+
+   $Message = "Mountpoint $($BLDriveC.MountPoint) is $($BLDriveC.VolumeStatus)"
+    LogMessage($Message)
+
+
 If ($BLDriveC.VolumeStatus -eq "FullyDecrypted")
 {
+   
+   $Message = "Bitlocker not enabled on C:!"
+    LogMessage($Message)
+
    #  Write-Host "False"
     return 1
 }
 else
 {
+   $Message = "Bitlocker enabled on C:"
+    LogMessage($Message)
+
     Write-host "True"
     return 0
 }
