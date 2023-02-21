239ad731ff205ac65768ad17456b466a41f021bb
henrywang08 <henry_ly@hotmail.com>
2023-02-21 14:16:10 +0800

    Create Start-BLWizard.ps1

---
+++ Start-BLWizard.ps1
@@ -0,0 +1,30 @@
+# This script will check if BitLocker is enabled or not. 
+# If not, it will start BitLocker Wizard for the user to complete
+# And update registry to state the complete status
+
+Function LogMessage($msg)
+{
+    $Timestamp = get-date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "$Timestamp $msg"
+}
+
+$BLDriveC = Get-BitLockerVolume -MountPoint c:
+
+$Message = "Mountpoint $($BLDriveC.MountPoint) is $($BLDriveC.VolumeStatus)"
+LogMessage($Message)
+
+if ($BLDriveC.VolumeStatus -eq "FullyDecrypted")
+{
+    $Message = "Start BitLocker Wizard"
+    LogMessage($Message)
+    
+    Start-Process -FilePath "c:\windows\system32\BitLockerWizardElev.exe" `
+        -ArgumentList "c: S" `
+        -Wait
+    
+    $BLDriveCAgain = Get-BitLockerVolume -MountPoint c:
+    
+    $Message = "BitLocker Wizard Result: Mountpoint $($BLDriveC.MountPoint) is $($BLDriveC.VolumeStatus)"
+     LogMessage($Message)
+}
+
