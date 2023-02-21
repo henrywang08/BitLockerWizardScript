aaa321cef2ff40501df273043f842e697ab54649
henrywang08 <henry_ly@hotmail.com>
2023-02-21 15:37:50 +0800

    Add files via upload

---
+++ BitLockerWizardforUser.ps1
@@ -0,0 +1,33 @@
+﻿# This script will check if BitLocker is enabled or not. 
+# If not, it will start BitLocker Wizard for the user to complete
+# And update registry to state the complete status
+
+$LogFile = "c:\windows\temp\blwizard.log"
+
+Function LogMessage($msg)
+{
+    $Timestamp = get-date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "$Timestamp $msg" 
+    "$Timestamp $msg" |     Out-File -FilePath $LogFile -Append 
+}
+
+$BLDriveC = Get-BitLockerVolume -MountPoint c:
+
+$Message = "Mountpoint $($BLDriveC.MountPoint) is $($BLDriveC.VolumeStatus)"
+LogMessage($Message)
+
+if ($BLDriveC.VolumeStatus -eq "FullyDecrypted")
+{
+    $Message = "Start BitLocker Wizard"
+    LogMessage($Message)
+    
+    Start-Process -FilePath "c:\windows\system32\BitLockerWizardElev.exe" `
+        -ArgumentList "c: S" `
+        -Wait
+    
+    $BLDriveCAgain = Get-BitLockerVolume -MountPoint c:
+    
+    $Message = "BitLocker Wizard Result: Mountpoint $($BLDriveC.MountPoint) is $($BLDriveC.VolumeStatus)"
+     LogMessage($Message)
+}
+
---
+++ BitLokerDetection.ps1
@@ -0,0 +1,12 @@
+﻿$BLDriveC = Get-BitLockerVolume c:
+If ($BLDriveC.VolumeStatus -eq "FullyDecrypted")
+{
+   #  Write-Host "False"
+    return 1
+}
+else
+{
+    Write-host "True"
+    return 0
+}
+
