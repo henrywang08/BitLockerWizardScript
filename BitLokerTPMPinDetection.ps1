80e47e6ac12ba877390ac50ad104103c0f7c072f
henrywang08 <henry_ly@hotmail.com>
2023-03-06 11:16:33 +0800

    Create BitLokerTPMPinDetection.ps1

---
+++ BitLokerTPMPinDetection.ps1
@@ -0,0 +1,19 @@
+﻿$BLDriveC = Get-BitLockerVolume c:
+If ($BLDriveC.VolumeStatus -ne "FullyDecrypted")
+{
+    if (($BLDriveC.KeyProtector).keyprotectortype -contains 'TpmPin')
+    {
+        Write-Host "True"
+        exit 0
+    }
+    else {
+        <# Action when all if and elseif conditions are false #>
+        exit
+    }
+}    
+else
+{
+    Write-host "True"
+    exit 0
+}
+
